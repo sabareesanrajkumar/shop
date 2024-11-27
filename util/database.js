edca0f0af9328e17b371a48e956071d30858a2e3
sabareesanrajkumar <sabareesanrajkumar05@gmail.com>
2024-11-27 18:49:17 +0530

    new branch

--- util/database.js
+++ util/database.js
@@ -1,9 +1,8 @@
-const mysql = require("mysql2");
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize("node-complete", "root", "9384776379@Sa", {
-  dialect: "mysql",
-  host: "localhost",
-  logging: false,
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
+  dialect: 'mysql',
+  host: 'localhost'
 });
 
 module.exports = sequelize;
