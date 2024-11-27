b2c82c2e58b37fe7d9b922605bec19b2c6a1fd45
sabareesanrajkumar <sabareesanrajkumar05@gmail.com>
2024-11-27 21:40:31 +0530

    many to many cart relationship

--- util/database.js
+++ util/database.js
@@ -1,8 +1,9 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
+const mysql = require("mysql2");
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize("node-complete", "root", "9384776379@Sa", {
+  dialect: "mysql",
+  host: "localhost",
+  logging: false,
 });
 
 module.exports = sequelize;
